14efbd61f589da65988a02b52b81713c5e0853c6
teamwsiz <pmarecki.pro@gmail.com>
2016-03-19 10:59:43 +0100

    Zmiana nazwy głównego .js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 //Tu wpisać położenie głónego źródła http, np. forum.wsi.edu.pl/path_abcd/
-var ROOTURL = 'http://x.xx.xxx.xxx/Forum/';
+var ROOTURL = 'http://#############/';
 var DEFAULT_CHANNEL = {cid:1, name:'wall', pass:''};
